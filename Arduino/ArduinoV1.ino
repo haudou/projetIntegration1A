927bad750a7ac12c881f18563453d5aebc8d6c61
haudoux <hugues.audoux@imerir.com>
2017-06-22 18:00:06 +0200

    Ajout Arduino V1

---
+++ Arduino/ArduinoV1.ino
@@ -0,0 +1,23 @@
+int baud = 9600;
+unsigned long EndClock = 1999;
+unsigned long TemponClock;
+unsigned long StartClock;
+int TimeGame = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(baud);
+  StartClock = millis();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  TemponClock = millis();
+  if(TemponClock - StartClock > EndClock){
+    TimeGame++;
+    Serial.print("0/Soleil/1/Soleil/");
+    Serial.println(TimeGame);
+    StartClock = millis();
+  } 
+}
+
